fc29ecb3fc6cdd10e953749f081c29e778975f4e
codeworkx <codeworkx@cyanogenmod.org>
2015-12-04 22:36:19 +0100

    g3: add initial quickcircle app
    picked from here: http://review.cyanogenmod.org/#/c/122794/y
    
    Change-Id: I88e5b037ca9241a85d504753203c9c74619e3e9f

--- g3.mk
+++ g3.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8974 \
     memtrack.msm8974 \
     liboverlay
+    
+# QuickCircle Case App
+PRODUCT_PACKAGES += \
+    QuickCircle
 
 # Glove mode
 PRODUCT_PACKAGES += \
