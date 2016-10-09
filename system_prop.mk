5ae29213c892d57b95d38fc99ca905bef1643d6e
yairpatch <yair.zarfati@gmail.com>
2016-10-09 18:53:43 +0300

    Bluetooth: set soc=smd
    Change-Id: I76ace88c8b50372a87bf28dae1da23b5a087b397

--- system_prop.mk
+++ system_prop.mk
@@ -19,8 +19,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    qcom.bluetooth.soc=rome \
-    ro.bt.bdaddr_path="/data/misc/bluetooth/bdaddr"
+    ro.bt.bdaddr_path="/data/misc/bluetooth/bdaddr" \
+    qcom.bluetooth.soc=smd
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
